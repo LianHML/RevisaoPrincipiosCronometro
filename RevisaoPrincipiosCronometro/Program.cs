7310019e310e68f8270f3e98f482753de149ef6b
Lian <lianlinsmeyer@gmail.com>
2022-08-02 15:53:54 -0300

    Criação do Menu e aplicação das funcionalidades

--- RevisaoPrincipiosCronometro/Program.cs
+++ RevisaoPrincipiosCronometro/Program.cs
@@ -7,10 +7,40 @@ namespace RevisaoPrincipiosCronometro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Quantos segundos devo contar?: ");
-            int tempoMaximo = int.Parse(Console.ReadLine());
-            Iniciar(tempoMaximo);
+            Menu();
         }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Cronômetro");
+            Console.WriteLine("");
+            Console.WriteLine("Devo contar em?: ");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Segundos");
+            Console.WriteLine("2 - Minutos");
+            Console.WriteLine("3 - Sair");
+            Console.WriteLine("");
+            int tipoTempo = int.Parse(Console.ReadLine());
+            if (tipoTempo == 3)
+                Sair();
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Cronômetro");
+                Console.WriteLine("");
+                Console.WriteLine("Quanto devo contar?: ");
+                Console.WriteLine("");
+            }
+            int tempoMaximo = int.Parse(Console.ReadLine());
+
+            switch (tipoTempo)
+            {
+                case 1: Iniciar(tempoMaximo); break;
+                case 2: Iniciar(tempoMaximo * 60); break;
+                default: Menu(); break;
+            }
+        }
+
         static void Iniciar(int tempoSelecionado)
         {
             int tempoAtual = 0;
@@ -18,10 +48,25 @@ namespace RevisaoPrincipiosCronometro
             while (tempoAtual != tempoSelecionado)
             {
                 Console.Clear();
+                Console.WriteLine("Cronômetro");
+                Console.WriteLine("");
                 tempoAtual++;
-                Console.WriteLine(tempoAtual);
+                Console.WriteLine($"Contagem: {tempoAtual}");
                 Thread.Sleep(1000);
             }
+            Console.Clear();
+            Console.WriteLine("Contagem Finalizada!");
+
+            Thread.Sleep(2000);
+            Menu();
         }
+        static void Sair()
+        {
+            Console.Clear();
+            Console.WriteLine("Obrigado! Até a próxima.");
+            Thread.Sleep(2500);
+            Environment.Exit(1);
+        }
+
     }
 }
